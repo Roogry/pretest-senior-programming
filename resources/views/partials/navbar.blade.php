4e8d822d76b4b252375b8b06b5957b0b6d50c60a
Roogry <jodiemantra@gmail.com>
2022-09-19 11:09:26 +0800

    change title

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light py-3">
     <div class="container">
-        <a class="navbar-brand fw-bolder" href="#">Uji Sertifikasi</a>
+        <a class="navbar-brand fw-bolder" href="#">Toko Berkah</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
